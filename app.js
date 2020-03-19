dcdbf46d218da222c85448ca11659eb6411b9a32
wuweixian <wwx@thetiger.com.cn>
2020-03-19 15:42:15 +0800

    合并

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ import Socket from './utils/webSocket.js'
 import {setConfig} from './env.config'
 App({
   onLaunch: function () {
+    // 获取系统参数，计算顶部栏高度
     this.globalData.systemInfo = wx.getSystemInfoSync();
+    this.globalData.systemInfo['titleHeight'] = this.getTitleHeight()
+    this.globalData.navBarHeight = this.globalData.systemInfo.statusBarHeight + this.getTitleHeight()
+
+    // 设置环境变量
     let {appId, envVersion} = wx.getAccountInfoSync().miniProgram
     let config = setConfig(appId, envVersion)
     Object.assign(this.globalData, config)
+
+    // 开启socket
     Socket.create({url: this.globalData.SOCKETPATH})
     Socket.onMessage((res) => {
     })
@@ -16,7 +23,14 @@ App({
   },
   globalData: {
     systemInfo: {},
+    navBarHeight: 0, // 顶部栏高度
     userInfo: null
+  },
+  // 计算标签栏高度
+  getTitleHeight () {
+    let rect = wx.getMenuButtonBoundingClientRect ? wx.getMenuButtonBoundingClientRect() : null; //胶囊按钮位置信息
+    let gap = rect.top - this.globalData.systemInfo.statusBarHeight; //动态计算每台手机状态栏到胶囊按钮间距
+    return 2 * gap + rect.height;
   },
   // 微信toast
   wxToast({title, icon = 'none', image, mask = true, duration = 2000, callback = function(){} }) {
--- components/layout/navBar/index.js
+++ components/layout/navBar/index.js
@@ -6,22 +6,17 @@ Component({
   properties: {
 
   },
-  attached () {
-    let systemInfo = app.globalData.systemInfo
-    let rect = wx.getMenuButtonBoundingClientRect ? wx.getMenuButtonBoundingClientRect() : null; //胶囊按钮位置信息
-      wx.getMenuButtonBoundingClientRect();
-      let navBarHeight = (function() { //导航栏高度
-              let gap = rect.top - systemInfo.statusBarHeight; //动态计算每台手机状态栏到胶囊按钮间距
-              return 2 * gap + rect.height;
-            })();
-  },
+ 
   /**
    * 组件的初始数据
    */
   data: {
-
+    titleHeight: app.globalData.systemInfo.titleHeight,
+    statusBarHeight: app.globalData.systemInfo.statusBarHeight
   },
-
+  ready () {
+    
+  },
   /**
    * 组件的方法列表
    */
