e77b5566c373ad4ba4e24a4d015360ccc8c16f7a
lizifa <lphvali@thetiger.com.cn>
2020-04-10 17:31:57 +0800

    修改

--- miniprogram/pages/homepage/index.js
+++ miniprogram/pages/homepage/index.js
@@ -46,7 +46,6 @@ Page({
   getUser() {
     return new Promise((resolve, reject) => {
       let { options, hasLogin } = this.data
-      let rtn = app.globalData.userInfo
       let callback = (data) => {
         let isOwner = hasLogin && options.vkey === app.globalData.userInfo.userInfo.vkey ? true : false
         let {
@@ -150,13 +149,11 @@ Page({
   chat() {
     let { PAGEPATH } = app.globalData
     let { options, httpCode } = this.data
-
     let jump = (options) => {
       wx.navigateTo({
         url: `${PAGEPATH}/IM/chat/index?vkey=${options.vkey}`
       })
     }
-
     if(httpCode === 2101) {
       app.wxConfirm({
         title: '取消拉黑',
